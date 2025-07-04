6fe6759b6187eae3556d070494ac76b65e2d85c1
WIWIEK ANDRIANI <andrianiwiwiek753@gmail.com>
2025-07-05 08:25:33 +0900

    Word-game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,42 @@
 class GamesController < ApplicationController
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @word = params[:word].to_s.upcase
+    @grid = params[:grid].to_s.split(',')
+
+    if !word_in_grid?(@word, @letters)
+      @result = "Sorry, #{@word} can't be builf form #{@letters.join(',')}"
+    elsif !english_word?(@word)
+      @result = "Sorry, #{@word} is not a valid English word"
+    else
+      @result = "Congratulations, #{@word} is valid!"
+    end
+  end
+
+  private
+
+  def word_in_grid?(word, grid)
+    grid_clone = grid.dup
+    word.chars.all? do |letter|
+      index = grid_clone.find_index(letter)
+      if index.nil?
+        false
+      else
+        grid_clone.delete_at(index)
+        true
+      end
+    end
+  end
+
+  def english_word?(word)
+    url = "http://wagon-dictionary.herokuapp.com/#{word.downcase}"
+    response = URI.open(url).read
+    json = JSON.parse(response)
+    json["found"]
+  rescue
+    false
+  end
 end
